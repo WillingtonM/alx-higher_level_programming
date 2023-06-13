c9a9d78ad76011bddd604dbec76680156701e936
WillingtonM <willington.mhlanga@gmail.com>
2023-06-13 09:10:31 +0200

    0x03-python-data_structures

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     new_str = ""
     for item in my_string:
-        if item is not 'c' and item is not 'C':
+        if item not in ('c', 'C'):
             new_str += item
     return (new_str)
