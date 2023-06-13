0cdfa7c5338a9684d81b1c52b6ad86ff6afb3713
WillingtonM <willington.mhlanga@gmail.com>
2023-06-13 09:20:53 +0200

    0x03-python-data_structures

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx > (len(my_list) ):
+    new_list = my_list[:]
+    ls_len = len(my_list)
+    if idx < 0 or idx >= ls_len:
         return (my_list)
 
-    list_copy = my_list[:]
-    list_copy[idx] = element
-    return (list_copy)
+    new_list[idx] = element
+    return (new_list)
