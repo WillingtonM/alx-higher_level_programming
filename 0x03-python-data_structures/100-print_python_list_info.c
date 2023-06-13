a64a59f7d51843f3af4183e6c6008c0e1c033acf
WillingtonM <willington.mhlanga@gmail.com>
2023-06-13 05:42:52 +0200

    python data structures

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -9,9 +9,9 @@ void print_python_list_info(PyObject *p)
     Py_ssize_t i, py_list_size;
     PyObject *item;
     const char *item_type;
-    PyListObject *list_object_cast;
 
     list_object_cast = (PyListObject *)p;
+    long int size = PyList_Size(p);
     py_list_size = PyList_Size(p);
 
     printf("[*] Size of the Python List = %d\n", (int)py_list_size);
@@ -22,4 +22,7 @@ void print_python_list_info(PyObject *p)
         item_type = Py_TYPE(item)->tp_name;
         printf("Element %d: %s\n", (int)i, item_type);
     }
+
+    for (index = 0; index < size; index++)
+        printf("Element %i: %s\n", index, Py_TYPE(obj->ob_item[index])->tp_name);
 }
