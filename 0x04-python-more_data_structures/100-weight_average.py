30312191211081bb35ff3561ba1dbea1de0339f0
WillingtonM <willington.mhlanga@gmail.com>
2023-06-15 02:03:27 +0200

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list and len(my_list):
+    if not isinstance(my_list, list) or len(my_list) == 0:
+        return (0)
         avr = sum(list(map(lambda x: x[0]*x[1], my_list)))
         sze = sum(list(map(lambda x: x[1], my_list)))
-        return (avg/size)
-    return 0
+        return (avg / sze)
