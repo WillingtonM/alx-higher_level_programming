7c433e59b6dceac9a41dd981c12690aeaefe8385
WillingtonM <willington.mhlanga@gmail.com>
2023-09-12 09:01:49 +0200

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,9 +2,8 @@
 if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
-  const arg_num = Number(process.argv[2]);
   let i = 0;
-  while (i < arg_num) {
+  while (i < Number(process.argv[2])) {
     console.log('C is fun');
     i++;
   }
