e1c005809a5fd0db1eb5938215c43a0e2e1b9483
WillingtonM <willington.mhlanga@gmail.com>
2023-09-12 23:33:51 +0200

    0x13. JavaScript - Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -12,7 +12,7 @@ class Rectangle {
   }
 
   print () {
-    for (let i = 0; i < this.height; i++) {
+    for (let x = 0; x < this.height; x++) {
       console.log(('X'.repeat(this.width)));
     }
   }
