5a137e9b5852ba1ff03d2cab845dd56a432916de
WillingtonM <willington.mhlanga@gmail.com>
2023-09-12 08:16:08 +0200

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-
-const arg_count = process.argv[2];
-
-if (arg_count === undefined) {
+if (process.argv[2] === undefined) {
   console.log('No argument');
 } else {
-  console.log(arg_count);
+  console.log(process.argv[2]);
 }
