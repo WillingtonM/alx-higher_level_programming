fe6037aebd56459e71509d861d52cf8b6883def7
WillingtonM <willington.mhlanga@gmail.com>
2023-09-12 10:52:01 +0200

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,20 +1,18 @@
 #!/usr/bin/node
+const my_array = process.argv
 
-function second (my_array) {
-  if (my_array.length === 2 || my_array.length === 3) { return (0); }
+if (my_array.length === 2 || my_array.length === 3) { return (0); }
 
-  let max_val = my_array[2];
-  let sec_max = my_array[3];
+let max_val = my_array[2];
+let sec_max = my_array[3];
 
-  for (let i = 2; i < my_array.length; i++) {
-    if (my_array[i] > max_val) {
-      sec_max = max_val;
-      max_val = my_array[i];
-    } else if (my_array[i] > sec_max && my_array[i] < max_val) {
-      sec_max = my_array[i];
-    }
+for (let i = 2; i < my_array.length; i++) {
+  if (my_array[i] > max_val) {
+    sec_max = max_val;
+    max_val = my_array[i];
+  } else if (my_array[i] > sec_max && my_array[i] < max_val) {
+    sec_max = my_array[i];
   }
-  return (sec_max);
 }
 
-console.log(second(process.argv));
+console.log(sec_max);
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-let my_array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-for (let i = 0, len = my_array.length; i < len; i++) {
-  console.log(my_array[i]);
+let my;
+for (i in my = ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(my[i]);
 }
