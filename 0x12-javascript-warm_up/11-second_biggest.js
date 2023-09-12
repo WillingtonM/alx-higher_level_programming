d24035cb7a8f99f0c8cee7d3ef89d54863118eea
WillingtonM <willington.mhlanga@gmail.com>
2023-09-12 10:53:15 +0200

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,17 +1,15 @@
 #!/usr/bin/node
-const my_array = process.argv
+if (process.argv.length === 2 || process.argv.length === 3) { return (0); }
 
-if (my_array.length === 2 || my_array.length === 3) { return (0); }
+let max_val = process.argv[2];
+let sec_max = process.argv[3];
 
-let max_val = my_array[2];
-let sec_max = my_array[3];
-
-for (let i = 2; i < my_array.length; i++) {
-  if (my_array[i] > max_val) {
+for (let i = 2; i < process.argv.length; i++) {
+  if (process.argv[i] > max_val) {
     sec_max = max_val;
-    max_val = my_array[i];
-  } else if (my_array[i] > sec_max && my_array[i] < max_val) {
-    sec_max = my_array[i];
+    max_val = process.argv[i];
+  } else if (process.argv[i] > sec_max && process.argv[i] < max_val) {
+    sec_max = process.argv[i];
   }
 }
 
