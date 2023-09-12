085904e041b02453299799950dbf960fc9167681
WillingtonM <willington.mhlanga@gmail.com>
2023-09-12 06:53:08 +0200

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
 
-const arg_count = process.argv.length;
-console.log(arg_count === 2 ? 'No argument' : arg_count === 3 ? 'Argument found' : 'Arguments found');
+console.log(process.argv.length === 2 ? 'No argument' : process.argv.length === 3 ? 'Argument found' : 'Arguments found');
