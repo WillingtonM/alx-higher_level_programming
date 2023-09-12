70e2aaa37166b8f586f783b9c57c3cc2fd9bbf7c
WillingtonM <willington.mhlanga@gmail.com>
2023-09-12 23:23:05 +0200

    0x13. JavaScript - Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-// Maps array and multiplies each element eith it's index
 const list = require('./100-data').list;
 console.log(list);
 console.log(list.map((num, elem) => num * elem));
--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-
 exports.esrever = function (list) {
   const rlist = [];
   for (let x = list.length - 1; x >= 0; x--) {
