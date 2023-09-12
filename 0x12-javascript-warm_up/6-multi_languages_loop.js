50e241ab153e78e7a1d43de2fd9af121b3bcd0f4
WillingtonM <willington.mhlanga@gmail.com>
2023-09-12 08:25:45 +0200

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,10 @@
 #!/usr/bin/node
-for (const ls_item of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
-  console.log(ls_item);
+let myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (const line of myArray) {
+  console.log(line);
+}
+
+for (let x = 0, arr_len = myArray.length; x < arr_len; x++) {
+  console.log(myArray[x]);
 }
