3d372a5233d294710aec7c5eb43abad13170c365
WillingtonM <willington.mhlanga@gmail.com>
2023-09-12 08:58:13 +0200

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-const size = Math.floor(Number(process.argv[2]));
-if (isNaN(size)) {
+const arg_numb = Math.floor(Number(process.argv[2]));
+if (isNaN(arg_numb)) {
   console.log('Missing size');
 } else {
-  for (let r = 0; r < size; r++) {
-    let row = '';
-    for (let c = 0; c < size; c++) row += 'X';
-    console.log(row);
+  for (let z = 0; z < arg_numb; z++) {
+    let xrow = '';
+    for (let y = 0; y < arg_numb; y++) xrow += 'X';
+    console.log(xrow);
   }
 }
