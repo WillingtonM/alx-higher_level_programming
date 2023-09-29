02b7ec2382c650f750fdbd6134034992632e53da
WillingtonM <willington.mhlanga@gmail.com>
2023-09-29 20:27:36 +0200

    0x11. Python - Network #1

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""get properties from mail"""
+"""get thy properties from mail"""
 
 if __name__ == '__main__':
-    import requests
     import sys
+    import requests
 
-    headers = {}
-    headers['Authorization'] = '{} {}'.format(sys.argv[0], sys.argv[1])
-    r = requests.get('https://api.github.com/users/{}'
-                     .format(sys.argv[1]), headers=headers)
-    print(r.json().get('id'))
+    req_headers = {}
+    req_headers['Authorization'] = '{} {}'.format(sys.argv[0], sys.argv[1])
+    req_res = requests.get('https://api.github.com/users/{}'
+                     .format(sys.argv[1]), req_headers=req_headers)
+    print(req_res.json().get('id'))
