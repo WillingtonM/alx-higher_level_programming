651a57426120086f73da9b279cc6596d134f7870
WillingtonM <willington.mhlanga@gmail.com>
2023-09-29 20:37:23 +0200

    0x11. Python - Network #1

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,9 +8,9 @@ import requests
 
 if __name__ == "__main__":
     req_repo = argv[1]
-    owner = argv[2]
+    rep_owner = argv[2]
     req_url = "https://api.github.com/repos/{}/{}/commits".format(
-        owner, req_repo)
+        rep_owner, req_repo)
 
     req_res = requests.get(req_url)
     commits = req_res.json()
