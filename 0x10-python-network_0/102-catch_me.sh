3e0346fbff894c26192a5cca20776c1e98bb7135
WillingtonM <willington.mhlanga@gmail.com>
2023-09-29 15:55:46 +0200

    0x10. Python - Network #0

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that makes request to 0.0.0.0:5000/catch_me & get response
-curl -sX PUT -L -d "user_id=98" -H "Origin: You got me!" "0.0.0.0:5000/catch_me"
+curl -sX GET 0.0.0.0:5000/catch_me
