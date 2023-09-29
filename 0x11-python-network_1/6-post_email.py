5f7fb56c683e88c7f0a1faa6740d1c70cf0a994a
WillingtonM <willington.mhlanga@gmail.com>
2023-09-29 21:24:21 +0200

    0x11. Python - Network #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-"""POST email using requests"""
+""" script that takes in URL and email address, sends
+    POST request to passed URL with email as parameter,
+    & finally displays body of response.
+
+    Usage: ./6-post_email.py <URL> <email>
+        Displays body of the response.
+"""
 import sys
 import requests
 
 if __name__ == "__main__":
-
     dta = {'email': sys.argv[2]}
     req_res = requests.post(sys.argv[1], dta=dta)
     print(req_res.text)
