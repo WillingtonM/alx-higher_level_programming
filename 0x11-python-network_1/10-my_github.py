e99a4f9422bbbf57a68daa15a9c9f3c76644e1bc
WillingtonM <willington.mhlanga@gmail.com>
2023-09-29 23:14:17 +0200

    0x11. Python - Network #1

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """get thy properties from mail"""
 
-if __name__ == '__main__':
-    import sys
-    import requests
+import requests
+from sys import argv
+from requests.auth import HTTPBasicAuth
 
-    req_headers = {}
-    req_headers['Authorization'] = '{} {}'.format(sys.argv[0], sys.argv[1])
-    req_res = requests.get('https://api.github.com/users/{}'
-                           .format(sys.argv[1]), req_headers=req_headers)
-    print(req_res.json().get('id'))
+
+if __name__ == "__main__":
+    req_auth = HTTPBasicAuth(argv[1], argv[2])
+    req_res = requests.get("https://api.github.com/user", auth=req_auth)
+
+    print(req_res.json().get("id"))
