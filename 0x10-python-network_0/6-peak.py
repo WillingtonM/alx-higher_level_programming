8ae69a5b38a2a45ebdde1c6dfd00cab0da8574f8
WillingtonM <willington.mhlanga@gmail.com>
2023-09-29 20:52:38 +0200

    0x10. Python - Network #0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,30 +1,19 @@
 #!/usr/bin/python3
 """Finds peak in list of unsorted integers"""
 
-
 def find_peak(list_of_integers):
-    """Find Peak in list
+    """
+        Find Peak in list
 
-    Args:
-        list_of_integers: list of int
-    Returns:
-        value or None
+        Args: list_of_integers: list of int
+        Returns: value or None
     """
 
-    if list_of_integers is None or list_of_integers == []:
-        return None
-    low_key     = 0
-    high_key    = len(list_of_integers)
-    mid_key     = ((high_key - lo) // 2) + lo
-    mid_key     = int(mid_key)
-    if high_key == 1:
-        return list_of_integers[0]
-    if high_key == 2:
-        return max(list_of_integers)
-    if list_of_integers[mid_key] >= list_of_integers[mid_key - 1] and\
-            list_of_integers[mid_key] >= list_of_integers[mid_key + 1]:
-        return list_of_integers[mid_key]
-    if mid_key > 0 and list_of_integers[mid_key] < list_of_integers[mid_key + 1]:
-        return find_peak(list_of_integers[mid_key:])
-    if mid_key > 0 and list_of_integers[mid_key] < list_of_integers[mid_key - 1]:
-        return find_peak(list_of_integers[:mid_key])
+    int_ls = list_of_integers
+    len_ls = len(int_ls)
+    if (len_ls <= 2):
+        return (None)
+    for z in range(1, len_ls - 1):
+	    if (int_ls[z - 1] <= int_ls[z] >= int_ls[z + 1]):
+	        return (int_ls[z])
+    return (None)
