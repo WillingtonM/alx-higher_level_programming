f854da56fa16f5ad8045521571c51586dca89d0f
WillingtonM <willington.mhlanga@gmail.com>
2023-09-29 20:20:16 +0200

    0x11. Python - Network #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""POST an email using requests"""
+"""POST email using requests"""
+import sys
+import requests
 
 if __name__ == "__main__":
-    import requests
-    import sys
 
-    data = {'email': sys.argv[2]}
-    r = requests.post(sys.argv[1], data=data)
-    print(r.text)
+    dta = {'email': sys.argv[2]}
+    req_res = requests.post(sys.argv[1], dta=dta)
+    print(req_res.text)
