b7640fa53a9a88c6cc741b9875694e7fa0eb1b63
WillingtonM <willington.mhlanga@gmail.com>
2023-10-24 21:28:20 +0200

    0x14. JavaScript - Web scraping

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,26 +2,17 @@
 // Script that prints all characters of Star Wars movie
 
 const request = require('request');
-
 const movieId = process.argv[2];
 const url = `https://swapi.dev/api/films/${movieId}/`;
-
-request.get(url, (err, resp, body) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-
-  const jsData = JSON.parse(body);
-  const chars = jsData.characters;
-  for (const character of chars) {
-    request(character, (err, resp, body) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      const charData = JSON.parse(body);
-      console.log(charData.name);
+request(url, (err, resp, body) => {
+  if (!err) {
+    const chars = JSON.parse(body).characters;
+    chars.forEach((character) => {
+      request(character, function (err, resp, body) {
+        if (!err) {
+          console.log(JSON.parse(body).name);
+        }
+      });
     });
   }
 });
